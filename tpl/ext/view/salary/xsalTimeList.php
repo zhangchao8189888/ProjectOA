699a01fc07460b62cb55f2e28a194dc6f2a4064b
filusilia <filus520@gmail.com>
2014-01-08 16:48:13 +0800

    normal update

--- tpl/ext/view/salary/xsalTimeList.php
+++ tpl/ext/view/salary/xsalTimeList.php
@@ -1,27 +1,39 @@
 <?php
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>工资列表查询</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link href="tpl/ext/lib/prettify/prettify.css" type="text/css" rel="stylesheet"/>
-    <link href="tpl/ext/resources/KitchenSink-all.css" rel="stylesheet"/>
-    <link href="common/css/admin.css" rel="stylesheet" type="text/css" />
-    <script language="javascript" type="text/javascript" src="common/ext/ext-all.js" charset="utf-8"></script>
-    <script language="javascript" type="text/javascript" src="common/ext/locale/ext-lang-zh_CN.js" charset="utf-8"></script>
-    <script language="javascript" type="text/javascript" src="tpl/ext/js/model.js" charset="utf-8"></script>
-    <script language="javascript" type="text/javascript" src="tpl/ext/js/data.js" charset="utf-8"></script>
-    <script language="javascript" type="text/javascript" src="common/js/jquery_last.js" charset="utf-8"></script>
-    <script type="text/javascript">
+<title>工资列表查询</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="tpl/ext/lib/prettify/prettify.css" type="text/css"
+	rel="stylesheet" />
+<link href="tpl/ext/resources/KitchenSink-all.css" rel="stylesheet" />
+<link href="common/css/admin.css" rel="stylesheet" type="text/css" />
+<script language="javascript" type="text/javascript"
+	src="common/ext/ext-all.js" charset="utf-8"></script>
+<script language="javascript" type="text/javascript"
+	src="common/ext/locale/ext-lang-zh_CN.js" charset="utf-8"></script>
+<script language="javascript" type="text/javascript"
+	src="tpl/ext/js/model.js" charset="utf-8"></script>
+<script language="javascript" type="text/javascript"
+	src="tpl/ext/js/data.js" charset="utf-8"></script>
+<script language="javascript" type="text/javascript"
+	src="common/js/jquery_last.js" charset="utf-8"></script>
+<script type="text/javascript">
         Ext.require([
             'Ext.grid.*',
             'Ext.toolbar.Paging',
             'Ext.data.*'
         ]);
         Ext.onReady(function(){
-
+            var md = new Ext.form.DateField({  
+                name:"testDate",  
+                editable:false, //不允许对日期进行编辑  
+                width:100,  
+                format:"Y-m-d",  
+                emptyText:"请选择日期..."  
+            });  
+            md.render('divDate');  
             //创建Grid
             var salTimeListGrid = Ext.create('Ext.grid.Panel',{
                 store: salTimeListstore,
@@ -137,9 +149,10 @@
                     },
                     '操作时间', {
                         id:'opTime',
-                        xtype : 'trigger',
-                        triggerClass : 'x-form-search-trigger',
-                        name: 'search',
+                        xtype : 'Date',
+                        width:300,
+                        bodyPadding:10,
+                        name: 'searchtime',
                         onTriggerClick : function(src) {
                             salTimeListstore.removeAll();
                             salTimeListstore.load( {
@@ -291,11 +304,11 @@
 </head>
 <body>
 <?php include("tpl/commom/top.html"); ?>
-<div id="main" style="min-width:960px">
+<div id="main" style="min-width: 960px">
     <?php include("tpl/commom/left.php"); ?>
     <div id="right">
-        <div id="demo"></div>
-    </div>
-</div>
+			<div id="demo"></div>
+		</div>
+	</div>
 </body>
 </html>
