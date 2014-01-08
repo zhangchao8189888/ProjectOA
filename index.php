4674b69c619cebdb069702a5833a7cc10d1cdc25
filusilia <filus520@gmail.com>
2014-01-08 17:04:47 +0800

    normal

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
-ob_start();
-session_start();
-require_once("./common/common.inc");
-$actionPath=$_REQUEST['action'];
-date_default_timezone_set("PRC");
-$user=$_SESSION['admin'];
-$loginusername=$usr['name'];
-if(empty($user)&&$_REQUEST['mode']!="checklogin"){
-	$actionrealpath="tpl/login.php";	
-}else{
+ob_start ();
+session_start ();
+require_once ("./common/common.inc");
+$actionPath = $_REQUEST ['action'];
+date_default_timezone_set ( "PRC" );
+$user = $_SESSION ['admin'];
+$loginusername = $usr ['name'];
+if (empty ( $user ) && $_REQUEST ['mode'] != "checklogin") {
+	$actionrealpath = "tpl/login.php";
+} else {
 	global $loginusername;
-   if(empty($actionPath)){
-  $actionrealpath="module/action/SalaryAction.class.php";
-  $actionPath="Salary";	
-  $firstLoginType=1;
-   }else{
-   	//这是
-   $actionrealpath="module/action/{$actionPath}Action.class.php";
-   }
+	if (empty ( $actionPath )) {
+		$actionrealpath = "module/action/SalaryAction.class.php";
+		$actionPath = "Salary";
+		$firstLoginType = 1;
+	} else {
+		// 这是
+		
+		$actionrealpath = "module/action/{$actionPath}Action.class.php";
+	}
 }
-require_once($actionrealpath);
+require_once ($actionrealpath);
 ?>
