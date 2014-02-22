00bb17e16e0c5e9c4963ea327350d390bb2e63b6
zhangchao8189888 <zhangchao8189888@126.com>
2014-02-22 19:51:20 +0800

    修复小BUG。同步代码。

--- module/action/SaveSalaryAction.class.php
+++ module/action/SaveSalaryAction.class.php
@@ -544,17 +544,16 @@ class SaveSalaryAction extends BaseAction {
         $shebao = $_REQUEST ['shebao'];
         $caozuo = $_REQUEST ['caozuo'];
         $leibie = $_REQUEST ['leibie'];
-        $kefuName = $_REQUEST ['kefuName'];
         $companyName = $_REQUEST ['companyName'];
         $employName = $_REQUEST ['employName'];
         $employNumber = $_REQUEST ['employNumber'];
         $beizhu = $_REQUEST ['beizhu'];
-        $shenbao = $_REQUEST ['shenbao'];
-        $caozuorenming= $_SESSION ['admin'] ['name'];
+        $shenbao =  '等待受理';
+        $kefuName= $_SESSION ['admin'] ['name'];
         $this->jisuan = new sumSalary();
         $sum= $this->jisuan->getSumShebao($leibie,$shebao);
         $this->objDao = new SalaryDao ();
-        $this->objDao->setZengyuan ($kefuName,$companyName,$employName,$employNumber,$leibie,$shebao,$waiqu,$sum,$yongren,$caozuorenming,$shenbao,$beizhu,$caozuo);
+        $this->objDao->setZengyuan ($kefuName,$companyName,$employName,$employNumber,$leibie,$shebao,$waiqu,$sum,$yongren,null,$shenbao,$beizhu,$caozuo);
         exit ();
     }
 	
